81b7832e29b2693a5ff235dbeca3fb4706fbab25
Christoph Kretschmer <ckret@kretschmer-und-kretschmer.de>
2017-07-15 19:23:51 +0200

    Projektdateien hinzufügen.

---
+++ HashTag/ThisDocument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Tools.Word;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace HashTag
+{
+    public partial class ThisDocument
+    {
+        private void ThisDocument_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisDocument_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Vom VSTO-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisDocument_Startup);
+            this.Shutdown += new System.EventHandler(ThisDocument_Shutdown);
+        }
+
+        #endregion
+    }
+}
